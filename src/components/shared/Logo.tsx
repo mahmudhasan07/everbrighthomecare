8964894c34efca13ea1649410e475681c886b3c0
mahmudtrainer <mahmudhasan.mm07@gmail.com>
2025-03-11 16:06:44 +0600

    added2

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 const Logo = () => {
   return (
             <div className="flex justify-center items-center">
-              <Image
+              <img
                 alt="logo"
                 src="https://everbrighthomecare.co.uk/wp-content/uploads/2023/08/20240426_124633_0000-e1714395307274.png"
                 width={200}
